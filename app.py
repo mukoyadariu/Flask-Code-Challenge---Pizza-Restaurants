e1de327707307db6a7cb22b583fb094682af2205
mukoyadariu <mukoya.darius@student.moringaschool.com>
2023-09-26 11:42:32 +0300

    completed code challenge

--- app.py
+++ app.py
@@ -1,36 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import db, Restaurant, Pizza, RestaurantPizza
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza_restaurants.db'
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cookies.db'
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-db.init_app(app)
-
-@app.route('/restaurants', methods=['GET'])
-def get_restaurants():
-    restaurants = Restaurant.query.all()
-    restaurant_list = [{'id': r.id, 'name': r.name, 'address': r.address} for r in restaurants]
-    return jsonify(restaurant_list)
-
-@app.route('/restaurants/<int:id>', methods=['GET'])
-def get_restaurant(id):
-    restaurant = Restaurant.query.get(id)
-    if restaurant:
-        restaurant_data = {
-            'id': restaurant.id,
-            'name': restaurant.name,
-            'address': restaurant.address,
-            'pizzas': [{'id': p.id, 'name': p.name, 'ingredients': p.ingredients} for p in restaurant.pizzas]
-        }
-        return jsonify(restaurant_data)
-    else:
-        return jsonify({'error': 'Restaurant not found'}), 404
-
-# Implement other routes: DELETE /restaurants/:id, GET /pizzas, POST /restaurant_pizzas
+from routes import *
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,31 +1,40 @@
-# models.py
-
-# from app import db
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from app import db
 
 class Restaurant(db.Model):
-    __tablename__ = 'restaurants'
-    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
-    address = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.String(100))
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizza', backref='restaurants')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address
+        }
 
 class Pizza(db.Model):
-    __tablename__ = 'pizza'
-    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    ingredients = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    ingredients = db.Column(db.String(100))
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'ingredients': self.ingredients
+        }
 
 class RestaurantPizza(db.Model):
-    __tablename__ = 'restaurant_pizzas'
-    
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Float, nullable=False)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
 
-    restaurant = db.relationship('Restaurant', backref=db.backref('pizzas', lazy=True))
-    pizza = db.relationship('Pizza', backref=db.backref('restaurants', lazy=True))
+    def serialize(self):
+        return {
+            'id': self.id,
+            'price': self.price,
+            'restaurant_id': self.restaurant_id,
+            'pizza_id': self.pizza_id
+        }
